ffc9c832dd132535f777f660e6848b4487be610b
Craig Reynolds <cwr@red3d.com>
2014-09-13 17:56:59 -0700

    Add intial files from "lein new app lazy-predator"

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lazy-predator
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/lazy_predator/core_test.clj
@@ -0,0 +1,7 @@
+(ns lazy-predator.core-test
+  (:require [clojure.test :refer :all]
+            [lazy-predator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
